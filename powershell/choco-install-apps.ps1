adb8438c5b2f9343b71743f6b4afd4354cc7e302
sonu sharma <echosonusharma@gmail.com>
2022-09-10 16:35:17 +0530

    choco install script updated

--- powershell/choco-install-apps.ps1
+++ powershell/choco-install-apps.ps1
@@ -1,15 +1,20 @@
 
+## make sure to update your system if you had a fresh OS installed
 ## To fix "cannot be loaded because running scripts is disabled on this system" (run: 'get-executionpolicy' returns: Restricted) -->
 ## set-executionpolicy remotesigned
 
 # Run following to bypass "not digitally signed" issue (fixes per session only)
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
-$Packages = 'brave','googlechrome','mysql.workbench','mysql','mysql-cli','mongodb','mongodb-shell','mongodb-compass','pgadmin4','postgresql','wsl2','discord.install','microsoft-windows-terminal','postman',
+# microsoft-windows-terminal, mongodb install failed due to some issues
+
+$Packages = @('brave','googlechrome','mysql.workbench','mysql','mysql-cli','mongodb','mongodb-shell','mongodb-compass','pgadmin4','postgresql','wsl2','discord.install','microsoft-windows-terminal','postman',
 'vscode','docker-desktop','docker-cli','git.install','gimp','kdenlive','gh','nvm','nodejs','vlc','rust','golang','visualstudio-installer','steam-client','figma','obs-studio.install','qbittorrent','7zip',
-'warp','clementine','libreoffice-fresh','yarn','virtualbox'
+'warp','clementine','libreoffice-fresh','yarn','virtualbox')
 
 $chocoIsInstalled = Test-Path -Path "$env:ProgramData\Chocolatey" # Get-Command choco.exe -ErrorAction SilentlyContinue
+
+$chocoList = choco list -lai
  
 if(!$chocoIsInstalled) {
   write-host "Chocolatey not found! Installing Chocolatey ..."
@@ -17,11 +22,15 @@ if(!$chocoIsInstalled) {
   Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))      
 }
   
-ForEach ($PackageName in $Packages)
+foreach ($PackageName in $Packages)
 {
-  write-host "Installing $PackageName ..."
+  if(!!($chocoList | Select-String -Pattern $PackageName -SimpleMatch)) {
+    write-host "Package $PackageName is already installed! Skipping installation..."
+  } else {
+    write-host "Installation for package $PackageName started..."
 
-  choco install $PackageName -y
+    choco install $PackageName -y
+  }
 }
 
 write-host "All installations completed!"
