f4d72ce3a5c2da72e5d0903f19efb744d57a592c
sonu sharma <echosonusharma@gmail.com>
2022-12-14 02:12:53 +0530

    choco install script update

--- powershell/choco-install-apps.ps1
+++ powershell/choco-install-apps.ps1
@@ -1,4 +1,4 @@
-## make sure to update your system if you had a fresh OS installed
+## make sure to update your system whether you had a fresh OS installed or not.
 ## to run the script run command - powershell -File <location of this file>
 ## To fix "cannot be loaded because running scripts is disabled on this system" (run: 'get-executionpolicy' returns: Restricted) -->
 ## set-executionpolicy remotesigned
@@ -8,16 +8,20 @@ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
 # microsoft-windows-terminal, mongodb install failed due to some issues, do manual installs if it fails
 
-$Packages = @('brave','googlechrome','mysql.workbench','mysql','mysql-cli','mongodb','mongodb-shell','mongodb-compass','pgadmin4','postgresql','wsl2','discord.install','microsoft-windows-terminal','postman',
-'vscode','docker-desktop','docker-cli','git.install','gimp','kdenlive','gh','nvm','nodejs','vlc','rust','golang','visualstudio-installer','steam-client','figma','obs-studio.install','qbittorrent','7zip',
-'warp','clementine','libreoffice-fresh','yarn','virtualbox', 'starship', 'crystaldiskinfo', 'mingw', 'fiddler')
+# list of all the packages
+$Packages = @('brave','googlechrome','mysql.workbench','mysql','mysql-cli','mongodb','mongodb-shell',
+'mongodb-compass','pgadmin4','postgresql','wsl2','discord.install','microsoft-windows-terminal','postman',
+'vscode','docker-desktop','docker-cli','git.install','gimp','kdenlive','gh','nvm','nodejs','vlc','rust',
+'golang','visualstudio-installer','steam-client','figma','obs-studio.install','qbittorrent','7zip',
+'warp','clementine','libreoffice-fresh','yarn','virtualbox', 'starship', 'crystaldiskinfo', 'mingw', 
+'fiddler', 'webp', 'responsively', 'insomnia-rest-api-client')
 
 $chocoIsInstalled = Test-Path -Path "$env:ProgramData\Chocolatey" # Get-Command choco.exe -ErrorAction SilentlyContinue
  
 if(!$chocoIsInstalled) {
-  write-host "Chocolatey not found! Installing Chocolatey ..."
+  write-host "Chocolatey was not found in your system! Installing Chocolatey ..."
 
-  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))      
+  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))      
 }
 
 $chocoList = choco list -lai # lists all the installed choco packages
@@ -25,7 +29,7 @@ $chocoList = choco list -lai # lists all the installed choco packages
 foreach ($PackageName in $Packages)
 {
   if(!!($chocoList | Select-String -Pattern $PackageName -SimpleMatch)) {
-    write-host "Package $PackageName is already installed! Skipping installation..."
+    write-host "Package $PackageName is already installed! Skipping installation for it..."
   } else {
     write-host "Installation for package $PackageName started..."
 
@@ -33,4 +37,4 @@ foreach ($PackageName in $Packages)
   }
 }
 
-write-host "All installations completed!"
+write-host "All installations completed, enjoy! (●'◡'●)"
